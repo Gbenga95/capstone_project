3c2db414222948bb7c149052a9d5e9b267975ae3
Gbenga <gbengaemmanuel450@gmail.com>
2025-08-28 15:13:38 +0100

    Added viewsets with permissions and sentiments filtering

--- api/views.py
+++ api/views.py
@@ -1,3 +1,75 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django_filters.rest_framework import DjangoFilterBackend
+from api.models import Movie, Genre, Review, Rating
+from api.serializers import MovieSerializer, GenreSerializer, ReviewSerializer, RatingSerializer
+from django.db.models import Avg
 
-# Create your views here.
+class IsOwnerOrAdmin(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.user == request.user or request.user.is_staff
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def get_permissions(self):
+        if self.action in ['create', 'update', 'partial_update', 'destroy']:
+            return [permissions.IsAdminUser()]
+        return super().get_permissions()
+
+    @action(detail=True, methods=['get'])
+    def genres(self, request, pk=None):
+        movie = self.get_object()
+        genres = movie.genres.all()
+        serializer = GenreSerializer(genres, many=True)
+        return Response(serializer.data)
+
+    @action(detail=True, methods=['get'])
+    def average_rating(self, request, pk=None):
+        movie = self.get_object()
+        avg = movie.ratings.aggregate(Avg('stars'))['stars__avg'] or 0
+        return Response({'average_rating': round(avg, 2) if avg else 0})
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def get_permissions(self):
+        if self.action in ['create', 'update', 'partial_update', 'destroy']:
+            return [permissions.IsAdminUser()]
+        return super().get_permissions()
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['sentiment']
+
+    def get_permissions(self):
+        if self.action == 'destroy':
+            return [IsOwnerOrAdmin()]
+        return super().get_permissions()
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class RatingViewSet(viewsets.ModelViewSet):
+    queryset = Rating.objects.all()
+    serializer_class = RatingSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    @action(detail=False, methods=['get'], url_path='movie/(?P<movie_id>\\d+)/ratings')
+
+    def movie_ratings(self, request, movie_id=None):
+        ratings = Rating.objects.filter(movie_id=movie_id)
+        serializer = self.get_serializer(ratings, many=True)
+        return Response(serializer.data)
